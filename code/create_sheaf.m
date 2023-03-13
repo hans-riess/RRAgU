602d00af332a893426a47bcbd31b6bdd304ce3cb
hans-riess <hans.riess@duke.edu>
2023-03-13 16:43:57 -0400

    begin problem formulation

--- code/create_sheaf.m
+++ code/create_sheaf.m
@@ -10,14 +10,10 @@ Dv = 3;
 De = 3;
 
 %initialize X
-a_min = 0;
-a_max = 10;
-X_tarski = zeros(Dv,N);
+X0 = zeros(Dv,N);
 for i=1:N
-    X_tarski(:,i) = randi([a_min,a_max],Dv,1);
+    X0(:,i) = 10*rand(Dv,1);
 end
-
-X_tarski = [1,7,7;1,5,4;8,3,5];
 
 %initialize A
 A = zeros(De,Dv,N,N);
@@ -33,25 +29,17 @@ end
 %% HEAT EQUATION
 p = Inf;
 %run heat equation
-T = 5g0; % number of iterations
+T = 20; % number of iterations
 E_tarski = zeros(T,1);
-E_minimax = zeros(T,1);
 X_tarski = X0;
-X_minimax = X0;
 trace_tarski = zeros(Dv,N,T);
-trace_minimax = zeros(Dv,N,T);
 E_tarski(1) = dirichlet(A,X_tarski,p);
-E_minimax(1) = dirichlet(A,X_tarski,p);
 trace_tarski(:,:,1)=X_tarski;
-trace_minimax(:,:,1) = X_minimax;
 for t=2:T
     X_tarski = mpm_add(X_tarski,tarski_laplacian(A,X_tarski));
-    X_minimax = mpm_add(X_minimax,minimax_laplacian(A,X_minimax));
     trace_tarski(:,:,t) = X_tarski;
-    trace_minimax(:,:,t) = X_minimax;
     E_tarski(t) = dirichlet(A,X_tarski,p);
-    E_minimax(t) = dirichlet(A,X_minimax,p);
 end
 
 %plot the results
-plot(linspace(0,T-1,T),E_tarski,linspace(0,T-1,T),E_minimax)
+plot(linspace(0,T-1,T),E_tarski)
