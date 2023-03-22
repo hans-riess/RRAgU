5dcf3598677dbb2bb72e713ae5b68ec54736968e
hans-riess <hans.riess@duke.edu>
2023-03-22 18:48:20 -0400

    real progress

--- code/trajectories_loss.m
+++ code/trajectories_loss.m
@@ -1,5 +1,5 @@
 %% RANDOM NUMBER GENERATOR
-rng(1)
+% rng(1)
 
 %% RANDOM GRAPH
 D = 3; %dimension of matrix weights
@@ -20,46 +20,44 @@ G = graph(adj_matrix~=Inf);
 figure(1)
 plot(G)
 %% RANDOM SHEAF
-Dv = D;
-De = D;
 a_min = -10;
 a_max = 1;
 %initialize A
-A = zeros(De,Dv,N,N);
+A = zeros(D,D,N,N);
 for i=1:N
     for j=1:N
         if adj_matrix(i,j) ~= Inf
-            A(:,:,i,j) = randi([a_min,a_max],De,Dv);
+            A(:,:,i,j) = randi([a_min,a_max],D,D);
         else
-            A(:,:,i,j) = mp_zeros(De,Dv);
+            A(:,:,i,j) = mp_zeros(D,D);
         end
     end
 end
 %% HEAT EQUATION
 x_min = 0;
 x_max = 10;
-Dv = 3;
+D = 3;
 p = 1;
 %run heat equation
 T = 50; % number of iterations
 n_trials = 20; %number of trials
 E_tarski = zeros(T,1);
 iterations = linspace(0,T-1,T);
+trace_tarski = zeros(D,N,T,n_trials);
 figure; hold on
 for trial=1:n_trials
     %initialize X
-    X0 = zeros(Dv,N);
+    X0 = zeros(D,N);
     for i=1:N
-        X0(:,i) = randi([x_min,x_max],Dv,1);
+        X0(:,i) = randi([x_min,x_max],D,1);
     end
     X_tarski = X0;
-    trace_tarski = zeros(Dv,N,T);
-    E_tarski(1) = dirichlet(A,X_tarski,p);
-    trace_tarski(:,:,1)=X_tarski;
+    E_tarski(1) = energy_loss(A,X_tarski,p);
+    trace_tarski(:,:,1,trial)=X_tarski;
     for t=2:T
         X_tarski = mp_add(mpm_add(X_tarski,tarski_laplacian(A,X_tarski,adj_matrix)),mp_ones(D,N));
-        trace_tarski(:,:,t) = X_tarski;
-        E_tarski(t) = dirichlet(A,X_tarski,p);
+        trace_tarski(:,:,t,trial) = X_tarski;
+        E_tarski(t) = energy_loss(A,X_tarski,p);
     end
-    plot(iterations,E_tarski,'b') %plot energy curve
+    plot(iterations,E_tarski,'black') %plot loss curve
 end
